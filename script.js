12bd09cdc6dea63bfb0b692d8dbe714671c584a7
Didier_AGT <didieragt@gmail.com>
2025-09-25 12:59:00 -0500

    update oct

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const cdt = 1000000;
 const cajita_ahorro = 144790;
+const fondoTotal = 1600000;
+let arrCarro = [
+    {concepto: "Gasolina", valor:110000},
+    {concepto: "Parqueo", valor:0},
+    {concepto: "Lavado",valor:0}
+];
+let arrServicios = [
+    {concepto: "ETB", valor:79900},
+    {concepto: "Vanti", valor:17520},
+    {concepto: "Codensa", valor:58610},
+    {concepto: "Acueducto", valor:0}
+];
 let total_Ahorro = 0;
 let total_pre_mes = 0;
 let total_real_mes = 0;
 let aporte_D = 0;
 let aporte_G = 0;
 let saldo = 0;
+let total = 0;
 
 let tbody = document.getElementById('tbody');
 let total_pre = document.getElementById('total_pre');
@@ -15,7 +28,8 @@ let tr_aporte_G = document.getElementById('aporte40');
 let td_saldo = document.getElementById('saldo');
 let cajitaNu = document.getElementById('cajitaNu');
 let cdtNu = document.getElementById('cdtNu');
-let totalNu = document.getElementById('totalNu');
+let totalAhorro = document.getElementById('totalAhorro');
+let fondo = document.getElementById('fondo');
 
 let formateador = new Intl.NumberFormat('es-CO', {
     style: 'currency',
@@ -30,7 +44,6 @@ async function data() {
         if(!response.ok){
             throw new Error(`HTTP error! status: ${response.status}`)
         }
-
         const datos = await response.json();
         datos.forEach(element => {
             total_pre_mes += element.presupuesto;
@@ -50,9 +63,19 @@ async function data() {
             tr.appendChild(tdEstado);
             tbody.appendChild(tr);
 
-            if(element.estado == 'pago'){
+            detalles(element.concepto,tdGastoReal,tdConcepto);
+
+            if(element.gasto_real == element.presupuesto || total == element.presupuesto){
                 tr.style.background = 'green';
                 tr.style.color = '#fff';
+                tdEstado.textContent = "COMPLETO";
+            } else if(element.gasto_real > element.presupuesto || total > element.presupuesto){
+                console.log(total);
+                tr.style.background = 'red';
+                tr.style.color = '#fff';
+                tdEstado.textContent = "SOBRECOSTO";
+            } else{
+                tdEstado.textContent = "PENDIENTE";
             }
         });
 
@@ -88,11 +111,62 @@ async function data() {
 
         cajitaNu.textContent = formateador.format(cajita_ahorro);
         cdtNu.textContent = formateador.format(cdt);
-        totalNu.textContent = formateador.format(cdt + cajita_ahorro);
+        fondo.textContent = formateador.format(fondoTotal);
+        totalAhorro.textContent = formateador.format(cdt + cajita_ahorro + fondoTotal);
 
     } catch (error) {
             console.error('Error al leer el archivo JSON:', error);
         }
+}
+
+function detalles(detalleConcepto,tdGastoReal,tdConcepto){
+
+    if(detalleConcepto == "Carro" || detalleConcepto == "Servicios"){
+        let detalles = document.createElement('a');
+        detalles.textContent = "detalles";
+        detalles.href = "#";
+        detalles.style.display = "block";
+        detalles.style.marginTop = "5px";
+
+        let tarjeta = document.createElement('div');
+        tarjeta.style.display = "none";
+        tarjeta.style.padding = "10px";
+        tarjeta.style.marginTop = "8px";
+        tarjeta.style.border = "1px solid #ccc";
+        tarjeta.style.borderRadius = "6px";
+        tarjeta.style.backgroundColor = "#000";
+        tarjeta.style.color = "#fff"
+
+        if(detalleConcepto == "Carro"){
+            let contenido =``;
+            arrCarro.forEach(item => {
+                contenido += `<p>${item.concepto}: | ${formateador.format(item.valor)}</p>`;
+                total += item.valor;
+                tarjeta.innerHTML = contenido;
+                tdGastoReal.textContent = formateador.format(total);
+            });
+        }
+
+        if(detalleConcepto == "Servicios"){
+            let contenido =``;
+            total = 0;
+            arrServicios.forEach(item => {
+                contenido += `<p>${item.concepto}: | ${formateador.format(item.valor)}</p>`;
+                total += item.valor;
+                tarjeta.innerHTML = contenido;
+                tdGastoReal.textContent = formateador.format(total);
+            });
+        }
+
+        detalles.addEventListener('click', function(e){
+            e.preventDefault();
+            tarjeta.style.display = tarjeta.style.display === "none" ? "block" : "none";
+        });
+
+        tdConcepto.appendChild(detalles);
+        tdConcepto.appendChild(tarjeta);
+    }
+
 }
 
 data();
